5c0416078e06e2a38e4d4d5706f3aee9fd66d394
Dylan <20106847@mail.wit.ie>
2025-11-25 11:55:54 +0000

    Mongo lab integrated Tasky React App

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,7 @@ import './db';
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ const errHandler = (err, req, res, next) => {
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.use(express.json());
 
